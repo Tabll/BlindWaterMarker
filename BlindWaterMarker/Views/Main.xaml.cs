cd0d4ab5708b154264fffa7bf82de53541aea275
Tabll <905153840@qq.com>
2019-04-11 19:48:10 +0100

    初始化项目

---
+++ BlindWaterMarker/Views/Main.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace BlindWaterMarker.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+
+            
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await PickFile();
+            
+        }
+
+        
+        // 选取图片
+        private async Task PickFile()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();  //新建文件选取器
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+
+                // Application now has read/write access to the picked file
+                this.textBlock.Text = "Picked photo: " + file.Name;
+            }
+            else
+            {
+                this.textBlock.Text = "Operation cancelled.";
+            }
+
+        }
+    }
+}
